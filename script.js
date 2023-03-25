bf44db8e09eb1504911add87c5dfc0b49fe8ac7d
Dairenji <37531253+ntokozokubheka@users.noreply.github.com>
2023-03-25 10:57:09 +0200

    updates

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-const slides= document.querySelector('slide');
-const left =getElementById('left');
-const right =getElementById('left');
-const SLIDE_COUNT =slides.length;
-let current_slides=0;
+const slides = document.querySelectorAll(".slide");
+const carousel = document.getElementById("carousel");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
 
+const SLIDES_COUNT = slides.length;
 
-left.addEventListener('click', () => {
-  
-  current_slide--;
+let current_slide = 0;
+
+left.addEventListener("click", () => {
+    current_slide--;
     if (current_slide < 0) {
         current_slide = SLIDES_COUNT - 1;
     }
-  
-    updateCarouse();
-  
-)};
 
-right.addEventListener('click', () => {
-  
-  current_slide++;
+    updateCarouse();
+});
+
+right.addEventListener("click", () => {
+    current_slide++;
     if (current_slide > SLIDES_COUNT - 1) {
         current_slide = 0;
     }
-  
-    updateCarouse();
-)};
 
-    function updateCarouse(){
-      
-      carousel.style.transform = `translateX(${
+    updateCarouse();
+});
+
+function updateCarouse() {
+    carousel.style.transform = `translateX(${
         -current_slide * slides[0].offsetWidth
     }px)`;
 
     document.body.style.background = `#${slides[current_slide].getAttribute(
         "data-bg"
     )}`;
-      
-    }
+}
